14f0d35dd95acf2e3b12e632d5deb4ac02a32532
Praphul <praphulkumar2023@gmail.com>
2024-11-18 14:37:15 +0530

    Removed: Extra comps

--- src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export default function Input({ type="number", label, identifier, handleChange, ...props }) {
-    return(
-        <p>
-            <label htmlFor="">{label}</label>
-            <input type={type} onChange={(e) => handleChange(identifier, e.target.value)} {...props}/>
-        </p>
-    )
-}
